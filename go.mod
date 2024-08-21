8d5ac52e1e7ec9f45e5ab3997d82c5da60e0d4e4
asamedeiros <andre.amedeiros@pagar.me>
2024-08-21 18:52:40 -0300

    update

--- go.mod
+++ go.mod
@@ -2,12 +2,35 @@ module github.com/asamedeiros/kong-go-sample-ddtrace
 
 go 1.22
 
-require github.com/Kong/go-pdk v0.11.0
+require (
+	github.com/Kong/go-pdk v0.11.0
+	go.opentelemetry.io/contrib/bridges/otelzap v0.0.0-20240821201842-57e55dcf509d
+	go.opentelemetry.io/otel v1.28.0
+	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp v0.4.0
+	go.opentelemetry.io/otel/log v0.4.0
+	go.opentelemetry.io/otel/sdk v1.28.0
+	go.opentelemetry.io/otel/sdk/log v0.4.0
+	go.uber.org/zap v1.27.0
+)
 
 require (
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240701130421-f6361c86f094 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240701130421-f6361c86f094 // indirect
+	google.golang.org/grpc v1.64.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 )
--- sample-ddtrace.go
+++ sample-ddtrace.go
@@ -1,18 +1,101 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
 
+	"github.com/asamedeiros/kong-go-sample-ddtrace/pkg/log"
 	"github.com/asamedeiros/kong-go-sample-ddtrace/plugin"
+	"go.opentelemetry.io/contrib/bridges/otelzap"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
+	"go.opentelemetry.io/otel/log/global"
+	_log "go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"go.uber.org/zap"
 
 	"github.com/Kong/go-pdk/server"
 )
 
+func newResource() (*resource.Resource, error) {
+	return resource.Merge(resource.Default(),
+		resource.NewWithAttributes(semconv.SchemaURL,
+			semconv.ServiceName("sample-ddtrace-plugin"),
+			semconv.ServiceVersion("0.1.0"),
+		))
+}
+
+func newLoggerProvider(ctx context.Context, res *resource.Resource) (*_log.LoggerProvider, error) {
+	exporter, err := otlploghttp.New(ctx)
+	if err != nil {
+		return nil, err
+	}
+	processor := _log.NewBatchProcessor(exporter)
+	provider := _log.NewLoggerProvider(
+		_log.WithResource(res),
+		_log.WithProcessor(processor),
+	)
+	return provider, nil
+}
+
+func setupLog(ctx context.Context) (*_log.LoggerProvider, log.Log) {
+
+	// Use a working LoggerProvider implementation instead e.g. use go.opentelemetry.io/otel/sdk/log.
+	//provider := noop.NewLoggerProvider()
+
+	// Create resource.
+	res, err := newResource()
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a logger provider.
+	// You can pass this instance directly when creating bridges.
+	loggerProvider, err := newLoggerProvider(ctx, res)
+	if err != nil {
+		panic(err)
+	}
+
+	// Register as global logger provider so that it can be accessed global.LoggerProvider.
+	// Most log bridges use the global logger provider as default.
+	// If the global logger provider is not set then a no-op implementation
+	// is used, which fails to generate data.
+	global.SetLoggerProvider(loggerProvider)
+
+	// Initialize a zap zaplogger with the otelzap bridge core.
+	// This method actually doesn't log anything on your STDOUT, as everything
+	// is shipped to a configured otel endpoint.
+	zaplogger := zap.New(otelzap.NewCore("sample-ddtrace", otelzap.WithLoggerProvider(loggerProvider)))
+
+	//zapLog, _ := zap.NewProduction(zap.AddCallerSkip(1))
+	//olog := otelzap.New(zapLog)
+	sugar := zaplogger.Sugar()
+
+	return loggerProvider, log.New(sugar)
+}
+
+func syncLog(ctx context.Context, loggerProvider *_log.LoggerProvider, l log.Log) {
+
+	l.Sync()
+	/* if err := l.Sync(); err != nil {
+		l.Warn("sync remaining logs failed: ", err.Error())
+	} */
+
+	if err := loggerProvider.Shutdown(ctx); err != nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
 
-	ctor := func() interface{} { return plugin.NewPlugin() }
+	ctx := context.Background()
+
+	logProvider, l := setupLog(ctx)
+	defer syncLog(ctx, logProvider, l)
+
+	ctor := func() interface{} { return plugin.NewPlugin(l) }
 	err := server.StartServer(ctor, plugin.Version, plugin.Priority)
 	if err != nil {
-		log.Printf("embedded plugin server start error: %w", err)
+		l.Error(fmt.Errorf("embedded plugin server start error: %w", err))
 	}
 }
